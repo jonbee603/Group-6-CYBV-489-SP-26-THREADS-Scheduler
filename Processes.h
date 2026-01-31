ba23b5bfa0ee007b96f84bac651cb535359ecaad
Cwmartin8 <cwmarti8@arizona.edu>
2026-01-30 20:24:44 -0700

    comment spacing

--- Processes.h
+++ Processes.h
@@ -10,17 +10,17 @@ typedef struct _process
 
 	char           name[MAXNAME];        /* Process name */
 	char           startArgs[MAXARG];    /* Process arguments */
-	void* context;              /* Process's current context */
+	void* context;						 /* Process's current context */
 	short          pid;                  /* Process id (pid) */
 	int            priority;			 /* Process can have priority 0-5, 5 being highest priority */
 	int (*entryPoint) (void*);           /* The entry point that is called from launch */
-	char* stack;				 /* For allocating stack memory */
+	char* stack;						 /* For allocating stack memory */
 	unsigned int   stacksize;			 /* Length/size of the stack */
 	int            status;               /* READY, QUIT, BLOCKED, etc. */
 	int			   processRunTime;       /* Total time process has run */
-	void* args;              /* argument passed to entryPoint */
-	int            exitCode;          /* this process's exit code */
-	int            waiting;           /* 1 if blocked in k_wait */
-	short          zombiePid;         /* child's pid ready to collect */
-	int            zombieExitCode;    /* child's exit code */
+	void* args;							 /* argument passed to entryPoint */
+	int            exitCode;			 /* this process's exit code */
+	int            waiting;              /* 1 if blocked in k_wait */
+	short          zombiePid;            /* child's pid ready to collect */
+	int            zombieExitCode;       /* child's exit code */
 } Process;
